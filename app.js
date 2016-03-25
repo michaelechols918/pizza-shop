55eb140e29dcd3031ee3fa1c46fe0484c7c368a2
Yoshishimi <michaelechols918@yahoo.com>
2016-03-25 11:51:04 -0700

    set up the tables but still adjusting

--- app.js
+++ app.js
@@ -27,46 +27,236 @@ function HourlyData(time, minPizzasSold, maxPizzasSold, minDeliveriesMade, maxDe
 };
 
 for (var i = 0; i < pizzaLocationArray.length; i++){
-  pizzaLocationArray[i].pushHourlyData(
+  pizzaLocationArray[0].pushHourlyData(
     new HourlyData('8:00 am', 0, 3, 1, 7)),
-  pizzaLocationArray[i].pushHourlyData(
+  pizzaLocationArray[0].pushHourlyData(
     new HourlyData('9:00 am', 0, 3, 1, 7)),
-  pizzaLocationArray[i].pushHourlyData(
+  pizzaLocationArray[0].pushHourlyData(
     new HourlyData('10:00 am', 0, 3, 1, 7)),
-    pizzaLocationArray[i].pushHourlyData(
+    pizzaLocationArray[0].pushHourlyData(
     new HourlyData('11:00 am', 5, 10, 2, 8)),
-    pizzaLocationArray[i].pushHourlyData(
+    pizzaLocationArray[0].pushHourlyData(
     new HourlyData('12:00 pm', 5, 10, 2, 8)),
-    pizzaLocationArray[i].pushHourlyData(
+    pizzaLocationArray[0].pushHourlyData(
     new HourlyData('1:00 pm', 5, 10, 2, 8)),
-    pizzaLocationArray[i].pushHourlyData(
+    pizzaLocationArray[0].pushHourlyData(
     new HourlyData('2:00 pm', 2, 13, 1, 7)),
-    pizzaLocationArray[i].pushHourlyData(
+    pizzaLocationArray[0].pushHourlyData(
     new HourlyData('3:00 pm', 2, 13, 1, 7)),
-    pizzaLocationArray[i].pushHourlyData(
+    pizzaLocationArray[0].pushHourlyData(
     new HourlyData('4:00 pm', 2, 13, 1, 7)),
-    pizzaLocationArray[i].pushHourlyData(
+    pizzaLocationArray[0].pushHourlyData(
     new HourlyData('5:00 pm', 0, 15, 2, 9)),
-    pizzaLocationArray[i].pushHourlyData(
+    pizzaLocationArray[0].pushHourlyData(
     new HourlyData('6:00 pm', 0, 15, 2, 9)),
-    pizzaLocationArray[i].pushHourlyData(
+    pizzaLocationArray[0].pushHourlyData(
     new HourlyData('7:00 pm', 0, 15, 2, 9)),
-    pizzaLocationArray[i].pushHourlyData(
+    pizzaLocationArray[0].pushHourlyData(
     new HourlyData('8:00 pm', 1, 3, 4, 12)),
-    pizzaLocationArray[i].pushHourlyData(
+    pizzaLocationArray[0].pushHourlyData(
     new HourlyData('9:00 pm', 1, 3, 4, 12)),
-    pizzaLocationArray[i].pushHourlyData(
+    pizzaLocationArray[0].pushHourlyData(
     new HourlyData('10:00 pm', 1, 3, 4, 12)),
-    pizzaLocationArray[i].pushHourlyData(
+    pizzaLocationArray[0].pushHourlyData(
     new HourlyData('11:00 pm', 8, 15, 6, 16)),
-    pizzaLocationArray[i].pushHourlyData(
+    pizzaLocationArray[0].pushHourlyData(
     new HourlyData('12:00 am', 8, 15, 6, 16)),
-    pizzaLocationArray[i].pushHourlyData(
+    pizzaLocationArray[0].pushHourlyData(
     new HourlyData('1:00 am', 8, 15, 6, 16));
+};
+for (var i = 0; i < pizzaLocationArray.length; i++){
+  pizzaLocationArray[1].pushHourlyData(
+    new HourlyData('8:00 am', 1, 3, 1, 7)),
+  pizzaLocationArray[1].pushHourlyData(
+    new HourlyData('9:00 am', 1, 3, 1, 7)),
+  pizzaLocationArray[1].pushHourlyData(
+    new HourlyData('10:00 am', 1, 3, 1, 7)),
+    pizzaLocationArray[1].pushHourlyData(
+    new HourlyData('11:00 am', 5, 9, 2, 8)),
+    pizzaLocationArray[1].pushHourlyData(
+    new HourlyData('12:00 pm', 5, 9, 2, 8)),
+    pizzaLocationArray[1].pushHourlyData(
+    new HourlyData('1:00 pm', 5, 9, 2, 8)),
+    pizzaLocationArray[1].pushHourlyData(
+    new HourlyData('2:00 pm', 2, 13, 1, 6)),
+    pizzaLocationArray[1].pushHourlyData(
+    new HourlyData('3:00 pm', 2, 13, 1, 6)),
+    pizzaLocationArray[1].pushHourlyData(
+    new HourlyData('4:00 pm', 2, 13, 1, 6)),
+    pizzaLocationArray[1].pushHourlyData(
+    new HourlyData('5:00 pm', 18, 32, 3, 9)),
+    pizzaLocationArray[1].pushHourlyData(
+    new HourlyData('6:00 pm', 18, 32, 3, 9)),
+    pizzaLocationArray[1].pushHourlyData(
+    new HourlyData('7:00 pm', 18, 32, 3, 9)),
+    pizzaLocationArray[1].pushHourlyData(
+    new HourlyData('8:00 pm', 1, 3, 5, 12)),
+    pizzaLocationArray[1].pushHourlyData(
+    new HourlyData('9:00 pm', 1, 3, 5, 12)),
+    pizzaLocationArray[1].pushHourlyData(
+    new HourlyData('10:00 pm', 1, 3, 5, 12)),
+    pizzaLocationArray[1].pushHourlyData(
+    new HourlyData('11:00 pm', 8, 20, 6, 16)),
+    pizzaLocationArray[1].pushHourlyData(
+    new HourlyData('12:00 am', 8, 20, 6, 16)),
+    pizzaLocationArray[1].pushHourlyData(
+    new HourlyData('1:00 am', 8, 20, 6, 16));
+};
+for (var i = 0; i < pizzaLocationArray.length; i++){
+  pizzaLocationArray[2].pushHourlyData(
+    new HourlyData('8:00 am', 0, 4, 0, 4)),
+  pizzaLocationArray[2].pushHourlyData(
+    new HourlyData('9:00 am', 0, 4, 0, 4)),
+  pizzaLocationArray[2].pushHourlyData(
+    new HourlyData('10:00 am', 0, 4, 0, 4)),
+    pizzaLocationArray[2].pushHourlyData(
+    new HourlyData('11:00 am', 0, 7, 0, 4)),
+    pizzaLocationArray[2].pushHourlyData(
+    new HourlyData('12:00 pm', 0, 7, 0, 4)),
+    pizzaLocationArray[2].pushHourlyData(
+    new HourlyData('1:00 pm', 0, 7, 0, 4)),
+    pizzaLocationArray[2].pushHourlyData(
+    new HourlyData('2:00 pm', 2, 15, 1, 4)),
+    pizzaLocationArray[2].pushHourlyData(
+    new HourlyData('3:00 pm', 2, 15, 1, 4)),
+    pizzaLocationArray[2].pushHourlyData(
+    new HourlyData('4:00 pm', 2, 15, 1, 4)),
+    pizzaLocationArray[2].pushHourlyData(
+    new HourlyData('5:00 pm', 10, 26, 4, 6)),
+    pizzaLocationArray[2].pushHourlyData(
+    new HourlyData('6:00 pm', 10, 26, 4, 6)),
+    pizzaLocationArray[2].pushHourlyData(
+    new HourlyData('7:00 pm', 10, 26, 4, 6)),
+    pizzaLocationArray[2].pushHourlyData(
+    new HourlyData('8:00 pm', 8, 22, 7, 15)),
+    pizzaLocationArray[2].pushHourlyData(
+    new HourlyData('9:00 pm', 8, 22, 7, 15)),
+    pizzaLocationArray[2].pushHourlyData(
+    new HourlyData('10:00 pm', 8, 22, 7, 15)),
+    pizzaLocationArray[2].pushHourlyData(
+    new HourlyData('11:00 pm', 0, 2, 2, 8)),
+    pizzaLocationArray[2].pushHourlyData(
+    new HourlyData('12:00 am', 0, 2, 2, 8)),
+    pizzaLocationArray[2].pushHourlyData(
+    new HourlyData('1:00 am', 0, 2, 2, 8));
+};
+for (var i = 0; i < pizzaLocationArray.length; i++){
+  pizzaLocationArray[3].pushHourlyData(
+    new HourlyData('8:00 am', 0, 4, 0, 4)),
+  pizzaLocationArray[3].pushHourlyData(
+    new HourlyData('9:00 am', 0, 4, 0, 4)),
+  pizzaLocationArray[3].pushHourlyData(
+    new HourlyData('10:00 am', 0, 4, 0, 4)),
+    pizzaLocationArray[3].pushHourlyData(
+    new HourlyData('11:00 am', 0, 7, 0, 4)),
+    pizzaLocationArray[3].pushHourlyData(
+    new HourlyData('12:00 pm', 0, 7, 0, 4)),
+    pizzaLocationArray[3].pushHourlyData(
+    new HourlyData('1:00 pm', 0, 7, 0, 4)),
+    pizzaLocationArray[3].pushHourlyData(
+    new HourlyData('2:00 pm', 5, 15, 0, 4)),
+    pizzaLocationArray[3].pushHourlyData(
+    new HourlyData('3:00 pm', 5, 15, 0, 4)),
+    pizzaLocationArray[3].pushHourlyData(
+    new HourlyData('4:00 pm', 5, 15, 0, 4)),
+    pizzaLocationArray[3].pushHourlyData(
+    new HourlyData('5:00 pm', 25, 39, 13, 18)),
+    pizzaLocationArray[3].pushHourlyData(
+    new HourlyData('6:00 pm', 25, 39, 13, 18)),
+    pizzaLocationArray[3].pushHourlyData(
+    new HourlyData('7:00 pm', 25, 39, 13, 18)),
+    pizzaLocationArray[3].pushHourlyData(
+    new HourlyData('8:00 pm', 22, 36, 5, 22)),
+    pizzaLocationArray[3].pushHourlyData(
+    new HourlyData('9:00 pm', 22, 36, 5, 22)),
+    pizzaLocationArray[3].pushHourlyData(
+    new HourlyData('10:00 pm', 22, 36, 5, 22)),
+    pizzaLocationArray[3].pushHourlyData(
+    new HourlyData('11:00 pm', 5, 21, 16, 31)),
+    pizzaLocationArray[3].pushHourlyData(
+    new HourlyData('12:00 am', 5, 21, 16, 31)),
+    pizzaLocationArray[3].pushHourlyData(
+    new HourlyData('1:00 am', 5, 21, 16, 31));
+};
+for (var i = 0; i < pizzaLocationArray.length; i++){
+  pizzaLocationArray[4].pushHourlyData(
+    new HourlyData('8:00 am', 2, 7, 3, 5)),
+  pizzaLocationArray[4].pushHourlyData(
+    new HourlyData('9:00 am', 2, 7, 3, 5)),
+  pizzaLocationArray[4].pushHourlyData(
+    new HourlyData('10:00 am', 2, 7, 3, 5)),
+    pizzaLocationArray[4].pushHourlyData(
+    new HourlyData('11:00 am', 3, 8, 3, 8)),
+    pizzaLocationArray[4].pushHourlyData(
+    new HourlyData('12:00 pm', 3, 8, 3, 8)),
+    pizzaLocationArray[4].pushHourlyData(
+    new HourlyData('1:00 pm', 3, 8, 3, 8)),
+    pizzaLocationArray[4].pushHourlyData(
+    new HourlyData('2:00 pm', 1, 5, 1, 4)),
+    pizzaLocationArray[4].pushHourlyData(
+    new HourlyData('3:00 pm', 1, 5, 1, 4)),
+    pizzaLocationArray[4].pushHourlyData(
+    new HourlyData('4:00 pm', 1, 5, 1, 4)),
+    pizzaLocationArray[4].pushHourlyData(
+    new HourlyData('5:00 pm', 5, 13, 2, 4)),
+    pizzaLocationArray[4].pushHourlyData(
+    new HourlyData('6:00 pm', 5, 13, 2, 4)),
+    pizzaLocationArray[4].pushHourlyData(
+    new HourlyData('7:00 pm', 5, 13, 2, 4)),
+    pizzaLocationArray[4].pushHourlyData(
+    new HourlyData('8:00 pm', 22, 41, 15, 42)),
+    pizzaLocationArray[4].pushHourlyData(
+    new HourlyData('9:00 pm', 22, 41, 15, 42)),
+    pizzaLocationArray[4].pushHourlyData(
+    new HourlyData('10:00 pm', 22, 41, 15, 42)),
+    pizzaLocationArray[4].pushHourlyData(
+    new HourlyData('11:00 pm', 15, 20, 6, 21)),
+    pizzaLocationArray[4].pushHourlyData(
+    new HourlyData('12:00 am', 15, 20, 6, 21)),
+    pizzaLocationArray[4].pushHourlyData(
+    new HourlyData('1:00 am', 15, 20, 6, 21));
+};
+for (var i = 0; i < pizzaLocationArray.length; i++){
+  pizzaLocationArray[5].pushHourlyData(
+    new HourlyData('8:00 am', 0, 4, 0, 4)),
+  pizzaLocationArray[5].pushHourlyData(
+    new HourlyData('9:00 am', 0, 4, 0, 4)),
+  pizzaLocationArray[5].pushHourlyData(
+    new HourlyData('10:00 am', 0, 4, 0, 4)),
+    pizzaLocationArray[5].pushHourlyData(
+    new HourlyData('11:00 am', 0, 7, 0, 4)),
+    pizzaLocationArray[5].pushHourlyData(
+    new HourlyData('12:00 pm', 0, 7, 0, 4)),
+    pizzaLocationArray[5].pushHourlyData(
+    new HourlyData('1:00 pm', 0, 7, 0, 4)),
+    pizzaLocationArray[5].pushHourlyData(
+    new HourlyData('2:00 pm', 2, 15, 1, 4)),
+    pizzaLocationArray[5].pushHourlyData(
+    new HourlyData('3:00 pm', 2, 15, 1, 4)),
+    pizzaLocationArray[5].pushHourlyData(
+    new HourlyData('4:00 pm', 2, 15, 1, 4)),
+    pizzaLocationArray[5].pushHourlyData(
+    new HourlyData('5:00 pm', 6, 9, 5, 18)),
+    pizzaLocationArray[5].pushHourlyData(
+    new HourlyData('6:00 pm', 6, 9, 5, 18)),
+    pizzaLocationArray[5].pushHourlyData(
+    new HourlyData('7:00 pm', 6, 9, 5, 18)),
+    pizzaLocationArray[5].pushHourlyData(
+    new HourlyData('8:00 pm', 4, 8, 2, 5)),
+    pizzaLocationArray[5].pushHourlyData(
+    new HourlyData('9:00 pm', 4, 8, 2, 5)),
+    pizzaLocationArray[5].pushHourlyData(
+    new HourlyData('10:00 pm', 4, 8, 2, 5)),
+    pizzaLocationArray[5].pushHourlyData(
+    new HourlyData('11:00 pm', 2, 4, 3, 11)),
+    pizzaLocationArray[5].pushHourlyData(
+    new HourlyData('12:00 am', 2, 4, 3, 11)),
+    pizzaLocationArray[5].pushHourlyData(
+    new HourlyData('1:00 am', 2, 4, 3, 11));
 };
 console.log('LocationHourlyData');
 
-function makeFirstTable(ballard){
+function makeTable(ballard){
   var header = document.createElement('h1');
   header.textContent = ballard.name;
   document.getElementById('setUpTables').appendChild(header);
@@ -93,10 +283,10 @@ function makeFirstTable(ballard){
   }
   document.getElementById('setUpTables').appendChild(table);
 }
-makeFirstTable(pizzaLocationArray[0]);
-makeFirstTable(pizzaLocationArray[1]);
-makeFirstTable(pizzaLocationArray[2]);
-makeFirstTable(pizzaLocationArray[3]);
-makeFirstTable(pizzaLocationArray[4]);
-makeFirstTable(pizzaLocationArray[5]);
+makeTable(pizzaLocationArray[0]);
+makeTable(pizzaLocationArray[1]);
+makeTable(pizzaLocationArray[2]);
+makeTable(pizzaLocationArray[3]);
+makeTable(pizzaLocationArray[4]);
+makeTable(pizzaLocationArray[5]);
 var ballardTable = document.createElement('table');
